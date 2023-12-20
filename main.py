e5b6a7450e9296da6f51fa863d6a83be7c059f2d
Wallace <wlcs@ic.ufal.br>
2023-12-20 13:21:49 -0300

    loop adicionado

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ estado = ['P → Q', 'P']
 fim = 'Q'
 print(estado)
 
-estado = regras.ModusPonens(estado)
-estado = regras.ModusTollens(estado)
-estado = regras.SilogismoHipotetico(estado)
-estado = regras.SilogismoDisjuntivo(estado)
-estado = regras.ElimDuplaNegacao(estado)
+while fim not in estado:
+    
+    estado = regras.ModusPonens(estado)
+    estado = regras.ModusTollens(estado)
+    estado = regras.SilogismoHipotetico(estado)
+    estado = regras.SilogismoDisjuntivo(estado)
+    estado = regras.ElimDuplaNegacao(estado)
 
 print(estado)
