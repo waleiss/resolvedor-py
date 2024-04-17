1c43232d5ae537b52de5a05f333032f04544185d
Wallace <wlcs@ic.ufal.br>
2024-04-17 14:15:15 -0300

    fix main

--- main.py
+++ main.py
@@ -3,12 +3,10 @@ from regras import Regras as regras
 def aplicar_regra_e_verificar_memoria(regra, memoria, conclusao):
     memoria_anterior = memoria.copy()  # Faz uma cópia da memória antes da aplicação da regra
     memoria = regra(memoria)  # Aplica a regra na memória
-    if conclusao in memoria:
-        return memoria  # Se a conclusão desejada estiver na memória, retorna a memória atualizada
-    elif memoria_anterior != memoria:
-        return aplicar_regras(memoria, conclusao)  # Se houve mudança na memória, continua aplicando as regras
+    if conclusao in memoria and memoria_anterior != memoria:
+        return memoria  # Se a conclusão desejada estiver na memória e houve mudança na memória, retorna a memória atualizada
     else:
-        return None  # Se não houve mudança na memória, retorna None, indicando que não é possível chegar à conclusão desejada
+        return None  # Caso contrário, retorna None, indicando que a memória não foi atualizada
 
 def aplicar_regras(memoria, conclusao, regras_module):
     regras = [
@@ -19,10 +17,10 @@ def aplicar_regras(memoria, conclusao, regras_module):
         regras_module.ElimDuplaNegacao
     ]
     for regra in regras:
-        memoria_atualizada = aplicar_regra_e_verificar_memoria(regra, memoria, conclusao)
-        if memoria_atualizada:
-            return memoria_atualizada
-    return None
+        resultado = aplicar_regra_e_verificar_memoria(regra, memoria, conclusao)
+        if resultado:
+            return resultado  # Retorna a memória atualizada se a conclusão desejada for encontrada e a memória for atualizada
+    return memoria  # Retorna a memória sem alterações se nenhuma regra levar à conclusão desejada
 
 
 simbolos = ['→', '↔', '~', '∧', '∨']
@@ -32,7 +30,7 @@ memoria = ['P → Q', '~Q']
 conclusao = '~P'
 
 resultado = aplicar_regras(memoria, conclusao, regras)
-if resultado:
+if conclusao in resultado:
     print("Conclusão encontrada:", resultado)
 else:
     print("Não foi possível chegar à conclusão desejada.")
